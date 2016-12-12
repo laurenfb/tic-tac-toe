7000dcf6eba5db54957bbfb91aa6ac5e278a93f7
Trish Griego <pmgriego@gmail.com>
2016-12-12 14:33:12 -0800

    added some files like app.js that were part of an earlier project.

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// app.js
+// grabbed this from previous code. 
+
+import $ from 'jquery';
+
+$(document).ready(function() {
+  // Write your application code here
+});
--- src/game.js
+++ src/game.js
@@ -1,10 +1,15 @@
 // game.js
 
+// var Game = require('game').default;
+// var game = new Game();
+var Game = function() {};
 
+Game.prototype = {
 
+helloWorld: function() {return "hello world";}
 
-
+};
 
 
 // export default
-module.exports = Game;
+module.exports = Game; // do we need this?
