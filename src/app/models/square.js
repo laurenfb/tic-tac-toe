3093a191a34e540e09f77f945fe562f268630e02
laurenfb <laurenfriesbrink@gmail.com>
2016-12-19 13:24:08 -0800

    i think i made Square a model???

---
+++ src/app/models/square.js
@@ -0,0 +1,18 @@
+import Backbone from 'backbone';
+
+const Square = Backbone.Model.extend({
+  defaults: {
+    contents: "",
+    xAxis: 1,
+    yAxis: 1,
+  },
+
+  initialize: function(options) {
+    this.set("contents", options.name);
+    this.set("xAxis", options.xAxis);
+    this.set("yAxis", options.yAxis)
+  }
+
+});
+
+export default Square;
