c4a1703916f4699183fe6e911a9cf05a15cad45e
laurenfb <laurenfriesbrink@gmail.com>
2016-12-19 17:00:24 -0800

    added instance var nextTurn to Board collection

--- src/app/collections/board.js
+++ src/app/collections/board.js
@@ -2,7 +2,11 @@ import Backbone from 'backbone';
 import Square from 'app/models/square'
 
 const Board = Backbone.Collection.extend({
-  model: Square
+  model: Square,
+
+  initialize: function(){
+    this.nextTurn = 1;
+  }
 });
 
 export default Board;
